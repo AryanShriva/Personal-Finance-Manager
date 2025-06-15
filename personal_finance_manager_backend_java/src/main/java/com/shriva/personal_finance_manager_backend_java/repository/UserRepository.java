4845180db47075a50cec198812eea90425c9b020
Aryan Shriva <aryanshriva08@gmail.com>
2025-06-15 18:52:01 +0530

    pom files

--- personal_finance_manager_backend_java/src/main/java/com/shriva/personal_finance_manager_backend_java/repository/UserRepository.java
+++ personal_finance_manager_backend_java/src/main/java/com/shriva/personal_finance_manager_backend_java/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.shriva.personal_finance_manager_backend_java.repository;
 
+import com.shriva.personal_finance_manager_backend_java.model.User;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 }
