c97faf1166d8b3dd96e54b6122b9205229ac793e
Aryan Shriva <aryanshriva08@gmail.com>
2025-06-17 19:55:10 +0530

    application property fixes

--- personal_finance_manager_backend_java/src/main/java/com/shriva/personal_finance_manager_backend_java/PersonalFinanceManagerBackendJavaApplication.java
+++ personal_finance_manager_backend_java/src/main/java/com/shriva/personal_finance_manager_backend_java/PersonalFinanceManagerBackendJavaApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PersonalFinanceManagerBackendJavaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PersonalFinanceManagerBackendJavaApplication.class, args);
 	}
-
 }
