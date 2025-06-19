e6f641d0e4901a081420118c6b63ab534538e4aa
Aryan Shriva <aryanshriva08@gmail.com>
2025-06-19 19:11:11 +0530

    Transaction entity

--- personal_finance_manager_backend_java/src/main/java/com/shriva/personal_finance_manager_backend_java/model/Transaction.java
+++ personal_finance_manager_backend_java/src/main/java/com/shriva/personal_finance_manager_backend_java/model/Transaction.java
@@ -1,17 +1,41 @@
-//package com.shriva.personal_finance_manager_backend_java.model;
-//
-//@Entity
-//public class Transaction {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//    private Double amount;
-//    private String description;
-//    private Date date;
-//    private String type; // "INCOME" or "EXPENSE"
-//    @ManyToOne
-//    private Category category;
-//    @ManyToOne
-//    private User user;
-//    // Getters and setters
-//}
+package com.shriva.personal_finance_manager_backend_java.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "transactions")
+@Getter
+@Setter
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private Double amount;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TransactionType type;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    private String description;
+
+    public enum TransactionType {
+        INCOME, EXPENSE
+    }
+}
